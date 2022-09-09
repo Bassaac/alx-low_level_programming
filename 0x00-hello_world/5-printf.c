4e0120331195962c98b300fa067ebe832d075c8a
Bassaac <robertsisaacrobo@gmail.com>
2022-09-09 05:14:17 -0700

    new commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
-
 /**
  *
- * main - Entry point
- * Return: Always 0 (Success)
+ *  * main - Entry point
  *
- **/
-int main (void)
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
-	printf("with proper grammar, but the outcome is a piece of art,"/n
-		);
-return (0);
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,
+			\n");
+
+	return (0);
+
 }
