cdfcf7ee4db8379bfc7e55fc72a3723ceb5b7f6b
Bassaac <robertsisaacrobo@gmail.com>
2022-09-17 16:35:21 -0700

    new commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int num[9] = "123456789";
+
+	int i;
+
+
+
+	for (i = 0; i < 10; i++)
+
+	{
+
+		putchar(num[i]);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
