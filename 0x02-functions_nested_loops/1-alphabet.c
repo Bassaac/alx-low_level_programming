5fc41a5a4517c0c8291a6dec97d36ae90566e117
Bassaac <robertsisaacrobo@gmail.com>
2022-09-19 12:39:37 -0700

    new commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+/**
+ * print_alphabet - print alphabet of letter
+ *
+ * Return: void
+ */
+
+
+
+void print_alphabet(void)
+
+{
+
+	int i;
+
+
+
+	for (i = 'a'; i <= 'z'; i++)
+
+
+
+	{
+
+		_putchar(i);
+
+	}
+
+	_putchar('\n');
+
+}
