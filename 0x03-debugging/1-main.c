36ed5be26c4364d6b4ee3a260dd37d0a524c36bd
Bassaac <robertsisaacrobo@gmail.com>
2022-09-27 17:42:57 -0700

    new commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
+
 /**
  * main - causes an infinite loop
  * Return: 0
  */
+
+
 
 int main(void)
 
@@ -11,9 +14,15 @@ int main(void)
 
 	int i;
 
+
+
 	printf("Infinite loop incoming :(\n");
 
+
+
 	i = 0;
+
+
 
 	/*while (i < 10)*/
 
@@ -23,7 +32,9 @@ int main(void)
 
 	/*}*/
 
+
 	printf("Infinite loop avoided! \\o/\n");
+
 
 	return (0);
 
